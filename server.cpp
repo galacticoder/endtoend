b9c6f49454616147ab47d2d00e76d8e21df5c104
galacticoder <galacticoderr@gmail.com>
2024-07-03 00:29:50 -0400

    added breaks

--- server.cpp
+++ server.cpp
@@ -165,12 +165,12 @@ void broadcastFile(string& filepath, string& serverpath, const string& username,
                 cout << "file sent to user" << endl;
                 static const string yes = "User has accepted your file. File has been sent to user";
                 send(senderSocket, yes.c_str(), yes.length(), 0);
-                // break;
+                break;
             }
             else {
                 static const string no = "The user did not accept the file you have sent\n"; //istead of user say the username didnt accept the file you attempted to send
                 send(senderSocket, no.c_str(), no.length(), 0);
-                // break;
+                break;
             }
 
         }
