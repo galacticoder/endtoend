b86d761ab00226e31a6080c4486efafbb248e093
galacticoder <galacticoderr@gmail.com>
2024-07-02 23:27:17 -0400

    updated server

--- server.cpp
+++ server.cpp
@@ -159,10 +159,12 @@ void broadcastFile(string& filepath, string& serverpath, const string& username,
                 std::vector<uint8_t> fi2 = sendtoclient.readFile(serverpath); //file path is a string to the file path //error when reading the file
                 std::string encodedDataClient = sendtoclient.b64EF(fi2);
                 sendtoclient.sendBase64Data(clientSocket, encodedDataClient); //send encoded key
+                break;
             }
             else {
                 static const string no = "The user did not accept the file you have sent\n"; //istead of user say the username didnt accept the file you attempted to send
                 send(senderSocket, no.c_str(), no.length(), 0);
+                break;
             }
 
         }
