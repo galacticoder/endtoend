2f2623d556ab0b3bab2f5403387eb4c74e687d50
galacticoder <galacticoderr@gmail.com>
2024-07-20 11:33:17 -0400

    updated server

--- server.cpp
+++ server.cpp
@@ -671,11 +671,11 @@ void handleClient(int clientSocket, int serverSocket) {
                     exit(1);
                 }
 
-                else {
-                    // cout << fmt::format("Clients connected: ({})", clientsNamesStr) << endl;
-                    std::cout << "Disconnected client with same username" << endl;
-                    close(clientSocket);
-                }
+                // else {
+                //     // cout << fmt::format("Clients connected: ({})", clientsNamesStr) << endl;
+                //     std::cout << "Disconnected client with same username" << endl;
+                //     close(clientSocket);
+                // }
                 lenOfUser.clear();
                 // last execution of username exists
             }
